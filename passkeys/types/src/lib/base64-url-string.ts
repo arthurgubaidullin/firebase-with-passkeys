b71a1fb9eb50746f647ba5dd87b4cc3c1abfb416
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-26 19:59:57 +0000

    Fix codec

--- passkeys/types/src/lib/base64-url-string.ts
+++ passkeys/types/src/lib/base64-url-string.ts
@@ -1,12 +1,3 @@
 import * as t from 'io-ts';
-import { NonEmptyString } from 'io-ts-types';
 
-export interface Base64URLStringBrand {
-  readonly Base64URLString: unique symbol;
-}
-
-export const Base64URLString = t.brand(
-  NonEmptyString,
-  (_): _ is t.Branded<NonEmptyString, Base64URLStringBrand> => true,
-  'Base64URLString'
-);
+export const Base64URLString = t.string;
