5ed71c5952f597850aa1f0b3327ef3a4818a37c5
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-27 08:16:55 +0000

    Add function

--- auth/get-user-api/src/lib/get-user-api.ts
+++ auth/get-user-api/src/lib/get-user-api.ts
@@ -1,3 +1,11 @@
-export function authGetUserApi(): string {
-  return 'auth-get-user-api';
-}
+import { UserRecord } from 'firebase-admin/auth';
+import * as O from 'fp-ts/Option';
+import * as AuthRepository from '@firebase-with-passkeys/user-auth-repository';
+import * as functions from 'firebase-functions/v1';
+import { getUser as _getUser } from './get-user';
+
+export const getUser: (userId: string) => Promise<O.Option<UserRecord>> =
+  _getUser({
+    ...AuthRepository.get(),
+    ...functions.logger,
+  });
