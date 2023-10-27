b647d744b94a52532d782955c366abd4b91408be
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-27 08:35:10 +0000

    Move project

--- auth/get-user/api/vite.config.ts
+++ auth/get-user/api/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
-  cacheDir: '../../node_modules/.vite/auth-get-user-api',
+  cacheDir: '../../../node_modules/.vite/auth-get-user-api',
 
   plugins: [nxViteTsPaths()],
 
@@ -14,7 +14,7 @@ export default defineConfig({
 
   test: {
     globals: true,
-    cache: { dir: '../../node_modules/.vitest' },
+    cache: { dir: '../../../node_modules/.vitest' },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
