1c219f202dfe48bb70d91da8fb8cc68f3d7edca7
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-27 14:02:18 +0000

    Export types

--- passkeys/authenticator/repository/type/src/index.ts
+++ passkeys/authenticator/repository/type/src/index.ts
@@ -1,1 +1,5 @@
-export * from './lib/passkeys-authenticator-repository-type';
+export * from './lib/repository-type';
+export * from './lib/create-authenticator-type';
+export * from './lib/get-authenticator-type';
+export * from './lib/get-authenticators-type';
+export * from './lib/update-authenticator-type';
--- passkeys/authenticator/repository/type/src/lib/passkeys-authenticator-repository-type.ts
+++
@@ -1,3 +0,0 @@
-export function passkeysAuthenticatorRepositoryType(): string {
-  return 'passkeys-authenticator-repository-type';
-}
