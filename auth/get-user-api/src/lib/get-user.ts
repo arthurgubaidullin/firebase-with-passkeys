ab238513a188d1d7db2b0587d6134dcf84059905
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-27 08:27:44 +0000

    Refactor function

--- auth/get-user-api/src/lib/get-user.ts
+++ auth/get-user-api/src/lib/get-user.ts
@@ -2,17 +2,17 @@ import { UserRecord } from 'firebase-admin/auth';
 import * as O from 'fp-ts/Option';
 import * as TE from 'fp-ts/TaskEither';
 import * as TO from 'fp-ts/TaskOption';
-import { GetUser } from './get-user-type';
-import { Logger } from './logger-type';
 import { pipe } from 'fp-ts/function';
+import { GetUser } from './get-user-type';
+import { LogError } from './log-error';
 
 export const getUser =
-  (P: GetUser & Logger) =>
+  (P: GetUser & LogError) =>
   async (userId: string): Promise<O.Option<UserRecord>> =>
     pipe(
       async () => P.getUser(userId),
       TE.orElseFirstIOK((e) => () => {
-        P.error(e);
+        P.error(e.message, e);
       }),
       TO.fromTaskEither,
       (t) => t()
