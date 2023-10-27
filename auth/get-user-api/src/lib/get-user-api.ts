6ed545daf8589fa8bb9e17e16bb447f6cc796a5a
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-27 08:30:27 +0000

    Rename project

--- auth/get-user-api/src/lib/get-user-api.ts
+++ auth/get-user-api/src/lib/get-user-api.ts
@@ -1,6 +1,6 @@
 import { UserRecord } from 'firebase-admin/auth';
 import * as O from 'fp-ts/Option';
-import * as AuthRepository from '@firebase-with-passkeys/user-auth-repository';
+import * as AuthRepository from '@firebase-with-passkeys/auth-user-repository';
 import * as functions from 'firebase-functions/v1';
 import { getUser as _getUser } from './get-user';
 
