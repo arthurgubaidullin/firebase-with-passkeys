6077789401fbf903e8bbe012a11662ef85f8ef4d
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-27 13:56:14 +0000

    Export codecs

--- passkeys/types/src/index.ts
+++ passkeys/types/src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/authentication-response-json';
 export * from './lib/public-key-credential-request-options-json';
 export * from './lib/registration-response-json';
 export * from './lib/public-key-credential-creation-options-json';
+export * from './lib/authenticator-transport';
+export * from './lib/credential-device-type';
