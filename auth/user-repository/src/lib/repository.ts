6f48f6669aab9c31e149fc4f24a9a46614817502
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-27 07:54:03 +0000

    Add function

--- auth/user-repository/src/lib/repository.ts
+++ auth/user-repository/src/lib/repository.ts
@@ -1,3 +1,16 @@
-export function userAuthRepository(): string {
-  return 'user-auth-repository';
-}
+import { getAuth } from 'firebase-admin/auth';
+import * as E from 'fp-ts/Either';
+import * as TE from 'fp-ts/TaskEither';
+import { AuthRepository } from './repository-type';
+import { pipe } from 'fp-ts/function';
+
+export const get = (): AuthRepository => {
+  const auth = getAuth();
+  return {
+    getUser: async (userId) =>
+      pipe(
+        TE.tryCatch(() => auth.getUser(userId), E.toError),
+        (t) => t()
+      ),
+  };
+};
