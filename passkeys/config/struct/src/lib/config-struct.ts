70a6d4d113a14ced7f676d5bb6f8b2e983341942
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-27 17:26:31 +0000

    Add type

--- passkeys/config/struct/src/lib/config-struct.ts
+++ passkeys/config/struct/src/lib/config-struct.ts
@@ -1,6 +1,12 @@
 import * as t from 'io-ts';
 import { NonEmptyString } from 'io-ts-types';
 
+export interface UnsafeConfigStruct {
+  readonly NX_RP_NAME: string | undefined;
+  readonly NX_RP_ID: string | undefined;
+  readonly NX_ORIGIN: string | undefined;
+}
+
 export type ConfigStruct = t.TypeOf<typeof ConfigStruct>;
 
 export const ConfigStruct = t.readonly(
