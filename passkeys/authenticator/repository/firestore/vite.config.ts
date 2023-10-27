f0b9a4df2a375266669a23ede61de35871ab5e4f
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-27 13:48:27 +0000

    Move project

--- passkeys/authenticator/repository/firestore/vite.config.ts
+++ passkeys/authenticator/repository/firestore/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
-  cacheDir: '../../../node_modules/.vite/passkeys-authenticator-repository',
+  cacheDir: '../../../../node_modules/.vite/passkeys-authenticator-repository',
 
   plugins: [nxViteTsPaths()],
 
@@ -14,7 +14,7 @@ export default defineConfig({
 
   test: {
     globals: true,
-    cache: { dir: '../../../node_modules/.vitest' },
+    cache: { dir: '../../../../node_modules/.vitest' },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
