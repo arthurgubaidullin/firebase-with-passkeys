0eae212a3d07d9995abae36ea7130be16dcc345c
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-27 13:06:40 +0000

    Add function

--- passkeys/challenge/set-document/src/lib/passkeys-challenge-set-document.ts
+++ passkeys/challenge/set-document/src/lib/passkeys-challenge-set-document.ts
@@ -1,3 +1,9 @@
-export function passkeysChallengeSetDocument(): string {
-  return 'passkeys-challenge-set-document';
-}
+import { ChallengeDocument } from '@firebase-with-passkeys/passkeys-challenge-document';
+import { SetChallenge } from './set-challenge-type';
+
+export const setChallenge =
+  (P: SetChallenge) =>
+  async (userId: string, challenge: ChallengeDocument): Promise<void> => {
+    const data = ChallengeDocument.encode(challenge);
+    await P.setChallenge(userId, data);
+  };
