1815721f67e207c4698288e66e3d30caa2242df7
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-27 13:02:35 +0000

    Fix function

--- passkeys/challenge/get-document/src/lib/get-document.ts
+++ passkeys/challenge/get-document/src/lib/get-document.ts
@@ -1,14 +1,36 @@
 import { ChallengeDocument } from '@firebase-with-passkeys/passkeys-challenge-document';
+import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
+import * as TE from 'fp-ts/TaskEither';
+import * as TO from 'fp-ts/TaskOption';
+import { flow, identity, pipe } from 'fp-ts/function';
+import { failure } from 'io-ts/PathReporter';
 import { GetChallenge } from './get-document-type';
+import { LogError } from './log-error-type';
+
+const NOT_FOUND = 'Not found.' as const;
+
+const validateDocumentData = flow(ChallengeDocument.decode, E.mapLeft(failure));
 
 export const getChallenge =
-  (P: GetChallenge) =>
+  (P: GetChallenge & LogError) =>
   async (userId: string): Promise<O.Option<ChallengeDocument>> => {
-    const raw = await P.getChallenge(userId);
-    if (O.isNone(raw)) {
-      return O.none;
-    }
-    const data = ChallengeDocument.decode(raw);
-    return O.fromEither(data);
+    return pipe(
+      TE.tryCatch(async () => P.getChallenge(userId), E.toError),
+      TE.chainOptionKW(() => NOT_FOUND)(identity),
+      TE.chainEitherKW(validateDocumentData),
+      TE.orElseFirstIOK((error) => () => {
+        if (error instanceof Error) {
+          P.error('Failed to fetch the challenge document.', {
+            userId,
+            errors: [error],
+          });
+        }
+        if (Array.isArray(error)) {
+          P.error('Challenge document schema invalid.', { errors: error });
+        }
+      }),
+      TO.fromTaskEither,
+      (t) => t()
+    );
   };
