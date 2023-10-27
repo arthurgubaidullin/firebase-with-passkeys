d398f8ffdb15fbd09bf1778cf96a82cd0ff5bf35
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-27 17:40:18 +0000

    Add function

--- passkeys/config/get/api/src/lib/passkeys-get-config-api.ts
+++ passkeys/config/get/api/src/lib/passkeys-get-config-api.ts
@@ -1,3 +1,13 @@
-export function passkeysGetConfigApi(): string {
-  return 'passkeys-get-config-api';
-}
+import { GetConfig } from '@firebase-with-passkeys/passkeys-config-reader-type';
+import { ConfigStruct } from '@firebase-with-passkeys/passkeys-config-struct';
+import * as IOE from 'fp-ts/IOEither';
+import { pipe } from 'fp-ts/function';
+import { failure } from 'io-ts/PathReporter';
+
+export const getConfig = (P: GetConfig): IOE.IOEither<Error, ConfigStruct> =>
+  pipe(
+    IOE.fromIO(P.getConfig),
+    IOE.chainEitherK(ConfigStruct.decode),
+    IOE.mapLeft(failure),
+    IOE.mapLeft(() => new Error('Could not read configuration for Passkeys.'))
+  );
