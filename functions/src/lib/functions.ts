1c0ef3d0edc1ffabd5d2ff87d6d0cadd1da6a5db
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-28 18:44:33 +0000

    Add firebase fucntions

--- functions/src/lib/functions.ts
+++ functions/src/lib/functions.ts
@@ -1,7 +1,36 @@
-import { onRequest } from 'firebase-functions/v2/https';
-import * as logger from 'firebase-functions/logger';
+import { generateAuthenticationOptionsHandler } from '@firebase-with-passkeys/passkeys-generate-authentication-options-api';
+import { generateRegistrationOptionsHandler } from '@firebase-with-passkeys/passkeys-generate-registration-options-api';
+import { verifyAuthenticationResponseHandler } from '@firebase-with-passkeys/passkeys-verify-authentication-response-api';
+import { verifyRegistrationResponseHandler } from '@firebase-with-passkeys/passkeys-verify-registration-response-api';
+import { initializeApp } from 'firebase-admin/app';
+import { onCall } from 'firebase-functions/v1/https';
+import { pipe } from 'fp-ts/function';
+import { getProgram } from './program';
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
+initializeApp();
+
+const program = getProgram();
+
+export const generateRegistrationOptions = pipe(
+  program,
+  generateRegistrationOptionsHandler,
+  onCall
+);
+
+export const verifyRegistrationResponse = pipe(
+  program,
+  verifyRegistrationResponseHandler,
+  onCall
+);
+
+export const generateAuthenticationOptions = pipe(
+  program,
+  generateAuthenticationOptionsHandler,
+  onCall
+);
+
+export const verifyAuthenticationResponse = pipe(
+  program,
+  verifyAuthenticationResponseHandler,
+  onCall
+);
