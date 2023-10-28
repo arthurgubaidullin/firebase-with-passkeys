6e4bf3d9da70665a0991e592e95f93dbb5501474
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-28 17:20:16 +0000

    Add function

---
+++ passkeys/generate-registration-options/api/src/lib/generate-registration-options.ts
@@ -0,0 +1,76 @@
+import { GetUser } from '@firebase-with-passkeys/auth-service-type';
+import { AuthenticatorDocument } from '@firebase-with-passkeys/passkeys-authenticator-document';
+import { GetAuthenticators } from '@firebase-with-passkeys/passkeys-authenticator-repository-type';
+import { LogError } from '@firebase-with-passkeys/passkeys-challenge-get-document';
+import { SetChallenge } from '@firebase-with-passkeys/passkeys-challenge-repository-type';
+import { setChallenge } from '@firebase-with-passkeys/passkeys-challenge-set-document';
+import { GetConfig } from '@firebase-with-passkeys/passkeys-config-reader-type';
+import { getAuthenticatorDocuments } from '@firebase-with-passkeys/passkeys-get-authenticator-documents';
+import { getConfig } from '@firebase-with-passkeys/passkeys-get-config';
+import { PublicKeyCredentialCreationOptionsJSON } from '@firebase-with-passkeys/passkeys-types';
+import { generateRegistrationOptions as _generateRegistrationOptions } from '@simplewebauthn/server';
+import { PublicKeyCredentialCreationOptionsJSON as _PublicKeyCredentialCreationOptionsJSON } from '@simplewebauthn/typescript-types';
+import * as E from 'fp-ts/Either';
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+
+export const UNAUTHENTICATED = 'unauthenticated';
+
+export const FAILED_PRECONDITION = 'failed-precondition';
+
+export const generateRegistrationOptions =
+  (P: GetConfig & GetUser & SetChallenge & GetAuthenticators & LogError) =>
+  async (
+    data?: Readonly<{ uid?: string }>
+  ): Promise<
+    E.Either<
+      Error | typeof UNAUTHENTICATED | typeof FAILED_PRECONDITION,
+      _PublicKeyCredentialCreationOptionsJSON
+    >
+  > => {
+    const userId = data?.uid;
+
+    const _config = pipe(getConfig(P), (a) => a());
+
+    if (E.isLeft(_config)) {
+      return _config;
+    }
+
+    const config = _config.right;
+
+    if (!userId) {
+      return E.left(UNAUTHENTICATED);
+    }
+
+    const _user = P.getUser(userId);
+
+    const _authenticators: Promise<readonly AuthenticatorDocument[]> =
+      getAuthenticatorDocuments(P)(userId);
+
+    const user = await _user;
+    if (O.isNone(user)) {
+      return E.left(UNAUTHENTICATED);
+    }
+    if (!user.value.email) {
+      return E.left(FAILED_PRECONDITION);
+    }
+
+    const authenticators = await _authenticators;
+
+    const options = await _generateRegistrationOptions({
+      rpName: config.NX_RP_NAME,
+      rpID: config.NX_RP_ID,
+      userID: user.value.uid,
+      userName: user.value.email,
+      attestationType: 'none',
+      excludeCredentials: authenticators.map((authenticator) => ({
+        id: authenticator.credentialID,
+        type: 'public-key',
+        transports: authenticator.transports,
+      })),
+    });
+
+    await setChallenge(P)(user.value.uid, options);
+
+    return E.right(PublicKeyCredentialCreationOptionsJSON.encode(options));
+  };
