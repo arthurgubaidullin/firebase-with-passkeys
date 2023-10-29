0b5dd82b9c1ce667fab936a663ef07c409782cce
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-29 20:12:22 +0000

    Add method

--- auth/get-user/api/src/lib/get-user-api.ts
+++ auth/get-user/api/src/lib/get-user-api.ts
@@ -2,10 +2,11 @@ import { UserRecord } from 'firebase-admin/auth';
 import * as O from 'fp-ts/Option';
 import * as AuthRepository from '@firebase-with-passkeys/auth-service-firebase';
 import * as functions from 'firebase-functions/v1';
-import { getUser } from '@firebase-with-passkeys/auth-get-user';
+import { getUser, getUserByEmail } from '@firebase-with-passkeys/auth-get-user';
 
 interface GetUserApi {
   readonly getUser: (userId: string) => Promise<O.Option<UserRecord>>;
+  readonly getUserByEmail: (email: string) => Promise<O.Option<UserRecord>>;
 }
 
 export const getUserApi = (): GetUserApi => {
@@ -15,5 +16,6 @@ export const getUserApi = (): GetUserApi => {
   } as const;
   return {
     getUser: getUser(P),
+    getUserByEmail: getUserByEmail(P),
   };
 };
