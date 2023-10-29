fc63a002f8ee66251cf2b69506c0ea5267c1b751
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-29 20:09:35 +0000

    Rename file

--- auth/service/firebase/src/lib/repository-type.ts
+++ auth/service/firebase/src/lib/repository-type.ts
@@ -1,4 +1,4 @@
-import { GetUserByEmail } from './get-user-by-email';
+import { GetUserByEmail } from './get-user-by-email-type';
 import { GetUser } from './get-user-type';
 
 export type AuthRepository = GetUser & GetUserByEmail;
