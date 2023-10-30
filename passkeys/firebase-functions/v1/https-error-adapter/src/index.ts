8b16aae34d2777e5a7ae8c2958ba0cd689ab79f3
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-30 10:17:42 +0000

    Add project

---
+++ passkeys/firebase-functions/v1/https-error-adapter/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/passkeys-firebase-functions-v1-https-error-adapter';
---
+++ passkeys/firebase-functions/v1/https-error-adapter/src/lib/passkeys-firebase-functions-v1-https-error-adapter.ts
@@ -0,0 +1,3 @@
+export function passkeysFirebaseFunctionsV1HttpsErrorAdapter(): string {
+  return 'passkeys-firebase-functions-v1-https-error-adapter';
+}
