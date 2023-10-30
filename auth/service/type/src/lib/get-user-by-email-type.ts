ad3f5c4eea0b0c2d7001850addc6a39418674c83
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-30 12:15:01 +0000

    Fix types

--- auth/service/type/src/lib/get-user-by-email-type.ts
+++ auth/service/type/src/lib/get-user-by-email-type.ts
@@ -1,6 +1,6 @@
-import { UserRecord } from 'firebase-admin/auth';
+import { UserStruct } from '@firebase-with-passkeys/auth-user-struct';
 import * as O from 'fp-ts/Option';
 
 export interface GetUserByEmail {
-  readonly getUserByEmail: (userId: string) => Promise<O.Option<UserRecord>>;
+  readonly getUserByEmail: (userId: string) => Promise<O.Option<UserStruct>>;
 }
--- auth/service/type/src/lib/get-user-type.ts
+++ auth/service/type/src/lib/get-user-type.ts
@@ -1,6 +1,6 @@
-import { UserRecord } from 'firebase-admin/auth';
+import { UserStruct } from '@firebase-with-passkeys/auth-user-struct';
 import * as O from 'fp-ts/Option';
 
 export interface GetUser {
-  readonly getUser: (userId: string) => Promise<O.Option<UserRecord>>;
+  readonly getUser: (userId: string) => Promise<O.Option<UserStruct>>;
 }
