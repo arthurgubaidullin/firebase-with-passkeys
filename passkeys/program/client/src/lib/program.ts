8bb20bcaa546ea13a09bc6da13164eabb0c1bbf9
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-30 16:44:54 +0000

    Move project

--- passkeys/program/client/src/lib/program.ts
+++ passkeys/program/client/src/lib/program.ts
@@ -1,5 +1,5 @@
 import { getGenerateAuthenticationOptionsFunction } from '@firebase-with-passkeys/passkeys-generate-authentication-options-firebase-function-client';
-import { getGenerateRegistrationOptionsFirebaseFunction } from '@firebase-with-passkeys/passkeys-generate-registration-options-firebase-function-browser';
+import { getGenerateRegistrationOptionsFirebaseFunction } from '@firebase-with-passkeys/passkeys-generate-registration-options-firebase-function-client';
 import { HttpsCallable } from 'firebase/functions';
 
 type ClientProgram = Readonly<{
