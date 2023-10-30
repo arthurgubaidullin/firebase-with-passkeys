446f0466280d0cc468b2685c3de73881b80087cd
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-30 17:08:47 +0000

    Fix function

--- passkeys/program/client/src/lib/program.ts
+++ passkeys/program/client/src/lib/program.ts
@@ -1,15 +1,18 @@
 import { getGenerateAuthenticationOptionsFunction } from '@firebase-with-passkeys/passkeys-generate-authentication-options-firebase-function-client';
 import { getGenerateRegistrationOptionsFirebaseFunction } from '@firebase-with-passkeys/passkeys-generate-registration-options-firebase-function-client';
+import { getVerifyRegistrationResponseFirebaseFunction } from '@firebase-with-passkeys/passkeys-verify-registration-response-firebase-function-client';
 import { HttpsCallable } from 'firebase/functions';
 
 type ClientProgram = Readonly<{
   generateRegistrationOptions: HttpsCallable<unknown, unknown>;
   generateAuthenticationOptions: HttpsCallable<unknown, unknown>;
+  verifyRegistrationResponse: HttpsCallable<unknown, unknown>;
 }>;
 
 export const getClientPasskeysProgram = (): ClientProgram => {
   return {
     ...getGenerateAuthenticationOptionsFunction(),
     ...getGenerateRegistrationOptionsFirebaseFunction(),
+    ...getVerifyRegistrationResponseFirebaseFunction(),
   };
 };
