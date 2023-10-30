1b6b9818cb1e1e2e711bac9d54df12e1e838d5af
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-30 18:33:22 +0000

    Add function

--- firebase-app/client/src/lib/firebase-app-client.ts
+++ firebase-app/client/src/lib/firebase-app-client.ts
@@ -1,3 +1,23 @@
-export function firebaseAppClient(): string {
-  return 'firebase-app-client';
-}
+import {
+  FirebaseApp,
+  FirebaseOptions,
+  getApps,
+  initializeApp,
+} from 'firebase/app';
+
+const DEFAULT = '[DEFAULT]' as const;
+
+export const getOrInitializeApp = (
+  options: FirebaseOptions,
+  name?: string | undefined
+) => {
+  let app: FirebaseApp;
+  const _name = name ?? DEFAULT;
+  const _app = getApps().find((a) => a.name === _name);
+  if (_app) {
+    app = _app;
+  } else {
+    app = initializeApp(options, _name);
+  }
+  return app;
+};
