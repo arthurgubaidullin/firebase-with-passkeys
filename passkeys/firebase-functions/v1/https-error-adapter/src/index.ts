79f63fa98224f7247cffc3be7634308100d12e95
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-30 10:34:17 +0000

    Export function

--- passkeys/firebase-functions/v1/https-error-adapter/src/index.ts
+++ passkeys/firebase-functions/v1/https-error-adapter/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/passkeys-firebase-functions-v1-https-error-adapter';
+export * from './lib/from-event-or-error';
--- passkeys/firebase-functions/v1/https-error-adapter/src/lib/passkeys-firebase-functions-v1-https-error-adapter.ts
+++
@@ -1,3 +0,0 @@
-export function passkeysFirebaseFunctionsV1HttpsErrorAdapter(): string {
-  return 'passkeys-firebase-functions-v1-https-error-adapter';
-}
