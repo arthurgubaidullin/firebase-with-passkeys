04af2fedc16d15f3be9402693bcc798ce0c6c702
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-30 16:35:17 +0000

    Add function

---
+++ passkeys/program/firebase-functions/client/src/lib/program.ts
@@ -0,0 +1,15 @@
+import { getGenerateAuthenticationOptionsFunction } from '@firebase-with-passkeys/passkeys-generate-authentication-options-firebase-function-client';
+import { getGenerateRegistrationOptionsFirebaseFunction } from '@firebase-with-passkeys/passkeys-generate-registration-options-firebase-function-browser';
+import { HttpsCallable } from 'firebase/functions';
+
+type ClientProgram = Readonly<{
+  generateRegistrationOptions: HttpsCallable<unknown, unknown>;
+  generateAuthenticationOptions: HttpsCallable<unknown, unknown>;
+}>;
+
+export const getClientPasskeysProgram = (): ClientProgram => {
+  return {
+    ...getGenerateAuthenticationOptionsFunction(),
+    ...getGenerateRegistrationOptionsFirebaseFunction(),
+  };
+};
