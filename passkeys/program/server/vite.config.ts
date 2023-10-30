1939374204b5e339f9d3ef848a0bfb1071d33891
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-30 16:42:39 +0000

    Move project

--- passkeys/program/server/vite.config.ts
+++ passkeys/program/server/vite.config.ts
@@ -4,7 +4,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   cacheDir:
-    '../../../../node_modules/.vite/passkeys-program-firebase-functions-v1',
+    '../../../node_modules/.vite/passkeys-program-firebase-functions-v1',
 
   plugins: [nxViteTsPaths()],
 
@@ -15,7 +15,7 @@ export default defineConfig({
 
   test: {
     globals: true,
-    cache: { dir: '../../../../node_modules/.vitest' },
+    cache: { dir: '../../../node_modules/.vitest' },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
