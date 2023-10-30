0b566759517ed8db597f300799ed08cc7c49ed33
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-30 13:04:39 +0000

    Refactor

--- passkeys/generate-authentication-options/api/src/lib/generate-authentication-options.ts
+++ passkeys/generate-authentication-options/api/src/lib/generate-authentication-options.ts
@@ -1,34 +1,39 @@
 import { GetUserByEmail } from '@firebase-with-passkeys/auth-service-type';
+import { LogError } from '@firebase-with-passkeys/logger-type-server';
 import { GetAuthenticators } from '@firebase-with-passkeys/passkeys-authenticator-repository-type';
 import { SetChallenge } from '@firebase-with-passkeys/passkeys-challenge-repository-type';
 import { setChallenge } from '@firebase-with-passkeys/passkeys-challenge-set-document';
+import {
+  InvalidInput,
+  UserNotFound,
+} from '@firebase-with-passkeys/passkeys-event-types';
 import { getAuthenticatorDocuments } from '@firebase-with-passkeys/passkeys-get-authenticator-documents';
 import { PublicKeyCredentialRequestOptionsJSON } from '@firebase-with-passkeys/passkeys-types';
 import { generateAuthenticationOptions as _generateAuthenticationOptions } from '@simplewebauthn/server';
 import {
-  PublicKeyCredentialRequestOptionsJSON as _PublicKeyCredentialRequestOptionsJSON,
   PublicKeyCredentialDescriptorFuture,
+  PublicKeyCredentialRequestOptionsJSON as _PublicKeyCredentialRequestOptionsJSON,
 } from '@simplewebauthn/typescript-types';
 import * as E from 'fp-ts/Either';
-import { pipe } from 'fp-ts/function';
 import * as O from 'fp-ts/Option';
-import { failure } from 'io-ts/PathReporter';
+import { TaskEither } from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 import { RequestData } from './request-data';
-import { LogError } from '@firebase-with-passkeys/logger-type-server';
-
-export const FAILED_PRECONDITION = 'failed-precondition';
 
 export const generateAuthenticationOptions =
   (P: LogError & GetAuthenticators & SetChallenge & GetUserByEmail) =>
-  async (
+  (
     rawData: unknown
-  ): Promise<
-    E.Either<
-      typeof FAILED_PRECONDITION | string[],
-      _PublicKeyCredentialRequestOptionsJSON
-    >
-  > => {
-    const _data = pipe(rawData, RequestData.decode, E.mapLeft(failure));
+  ): TaskEither<
+    UserNotFound | InvalidInput,
+    _PublicKeyCredentialRequestOptionsJSON
+  > =>
+  async () => {
+    const _data = pipe(
+      rawData,
+      RequestData.decode,
+      E.mapLeft(InvalidInput.create)
+    );
     if (E.isLeft(_data)) {
       return _data;
     }
@@ -37,7 +42,7 @@ export const generateAuthenticationOptions =
     const u = await P.getUserByEmail(data.username);
 
     if (O.isNone(u)) {
-      return E.left(FAILED_PRECONDITION);
+      return E.left(new UserNotFound());
     }
 
     const authenticators = await getAuthenticatorDocuments(P)(data.username);
