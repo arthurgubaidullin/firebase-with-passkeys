3e0ecdd9da8d8d37d2d1230efdb1e4ff69e7741c
Artur Gubaidullin <lapteuh@gmail.com>
2023-10-30 17:16:23 +0000

    Move project

--- passkeys/functions/src/lib/functions.ts
+++ passkeys/functions/src/lib/functions.ts
@@ -1,7 +1,7 @@
 import { generateAuthenticationOptionsHandler } from '@firebase-with-passkeys/passkeys-generate-authentication-options-firebase-function-server';
 import { generateRegistrationOptionsHandler } from '@firebase-with-passkeys/passkeys-generate-registration-options-firebase-function-server';
 import { verifyAuthenticationResponseHandler } from '@firebase-with-passkeys/passkeys-verify-authentication-response-firebase-function-server';
-import { verifyRegistrationResponseHandler } from '@firebase-with-passkeys/passkeys-verify-registration-response-api';
+import { verifyRegistrationResponseHandler } from '@firebase-with-passkeys/passkeys-verify-registration-response-firebase-function-server';
 import { initializeApp } from 'firebase-admin/app';
 import { onCall } from 'firebase-functions/v1/https';
 import { pipe } from 'fp-ts/function';
