141054bc289b9a38b860b61574b9af4ec9dabd86
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-05 11:00:15 +0000

    Refactor

--- auth/get-user/api/src/lib/get-user-api.ts
+++ auth/get-user/api/src/lib/get-user-api.ts
@@ -1,15 +1,12 @@
 import { getUser, getUserByEmail } from '@firebase-with-passkeys/auth-get-user';
 import * as AuthRepository from '@firebase-with-passkeys/auth-repository-firebase-auth';
-import { UserStruct } from '@firebase-with-passkeys/auth-user-struct';
+import {
+  GetUser,
+  GetUserByEmail,
+} from '@firebase-with-passkeys/auth-service-type';
 import * as functions from 'firebase-functions/v1';
-import * as O from 'fp-ts/Option';
 
-interface GetUserApi {
-  readonly getUser: (userId: string) => Promise<O.Option<UserStruct>>;
-  readonly getUserByEmail: (email: string) => Promise<O.Option<UserStruct>>;
-}
-
-export const getUserApi = (): GetUserApi => {
+export const getUserApi = (): GetUser & GetUserByEmail => {
   const P = {
     ...AuthRepository.get(),
     ...functions.logger,
