19aab3d90043c58fe1e86267d5dc575b31caca55
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-05 12:48:52 +0000

    Add type

--- observable/type/src/lib/observable-type.ts
+++ observable/type/src/lib/observable-type.ts
@@ -1,3 +1,29 @@
-export function observableType(): string {
-  return 'observable-type';
+export interface ReadonlyObservable<T> {
+  readonly get: () => T;
+}
+
+export interface Observable<T> extends ReadonlyObservable<T> {
+  readonly set: (value: T) => void;
+}
+
+export interface CreateObservable {
+  readonly observable: <T>(initial: T) => Observable<T>;
+}
+
+export interface CreateAction {
+  readonly action: <F>(f: F) => F;
+}
+
+export interface OnBecomeObserved {
+  readonly onBecomeObserved: <T>(
+    observable: Observable<T>,
+    f: () => void
+  ) => void;
+}
+
+export interface OnBecomeUnobserved {
+  readonly onBecomeUnobserved: <T>(
+    observable: Observable<T>,
+    f: () => void
+  ) => void;
 }
