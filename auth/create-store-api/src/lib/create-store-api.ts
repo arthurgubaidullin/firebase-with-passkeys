ba0c3e3d07d6557acf8af5bc02299e12a9ed0f9c
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-05 13:22:28 +0000

    Fix function

--- auth/create-store-api/src/lib/create-store-api.ts
+++ auth/create-store-api/src/lib/create-store-api.ts
@@ -1,30 +1,29 @@
+import { createStore } from '@firebase-with-passkeys/auth-create-store';
 import * as AuthState from '@firebase-with-passkeys/auth-state';
 import {
   UserStruct,
   fromUserRecord,
 } from '@firebase-with-passkeys/auth-user-struct';
-import { getOrInitializeApp } from '@firebase-with-passkeys/get-or-initialize-firebase-app';
-import { FirebaseOptions } from 'firebase/app';
+import { ReadonlyObservable } from '@firebase-with-passkeys/observable-type';
+import { FirebaseApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { action, observable, onBecomeObserved, onBecomeUnobserved } from 'mobx';
-import { createStore } from '@firebase-with-passkeys/auth-create-store';
-import { ReadonlyObservable } from '@firebase-with-passkeys/observable-type';
 
 const onAuthStateChangedAdapter =
-  (config: FirebaseOptions) => (f: (user: O.Option<UserStruct>) => void) => {
-    const auth = getAuth(getOrInitializeApp(config));
+  (firebaseApp: FirebaseApp) => (f: (user: O.Option<UserStruct>) => void) => {
+    const auth = getAuth(firebaseApp);
     return onAuthStateChanged(auth, (user) =>
       pipe(user, O.fromNullable, O.map(fromUserRecord), f)
     );
   };
 
 export const createAuthStateStoreApi = (
-  config: FirebaseOptions
+  firebaseApp: FirebaseApp
 ): ReadonlyObservable<AuthState.AuthState> =>
   createStore({
-    onAuthStateChanged: onAuthStateChangedAdapter(config),
+    onAuthStateChanged: onAuthStateChangedAdapter(firebaseApp),
     observable: (a) => observable.box(a, { deep: false }),
     action,
     onBecomeObserved,
