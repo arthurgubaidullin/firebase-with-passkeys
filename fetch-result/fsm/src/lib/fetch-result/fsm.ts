93df733c748e43a487a4efd717a089ccfbec0de6
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-06 11:23:38 +0000

    Add type

--- fetch-result/fsm/src/lib/fetch-result/fsm.ts
+++ fetch-result/fsm/src/lib/fetch-result/fsm.ts
@@ -1,3 +1,33 @@
-export function fetchResultFsm(): string {
-  return 'fetch-result/fsm';
-}
+import * as FR from '@firebase-with-passkeys/fetch-result-type';
+import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+export type { FetchResult } from '@firebase-with-passkeys/fetch-result-type';
+
+export const initial = FR.initial;
+
+export const fetch = <E, A>(
+  r: FR.FetchResult<E, A>
+): O.Option<FR.FetchResult<E, A>> =>
+  pipe(
+    r,
+    O.fromPredicate(FR.isInitial),
+    O.map(() => FR.fetching)
+  );
+
+export const success =
+  <E, A>(a: A) =>
+  (r: FR.FetchResult<E, A>): O.Option<FR.FetchResult<E, A>> =>
+    pipe(
+      r,
+      O.fromPredicate(FR.isFetching),
+      O.map(() => FR.success(a))
+    );
+
+export const failure =
+  <E, A>(e: E) =>
+  (r: FR.FetchResult<E, A>): O.Option<FR.FetchResult<E, A>> =>
+    pipe(
+      r,
+      O.fromPredicate(FR.isFetching),
+      O.map(() => FR.failure(e))
+    );
