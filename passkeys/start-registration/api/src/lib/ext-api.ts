e3599abb67bce699303c49bbc7dd9738db7c3188
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-13 13:34:43 +0000

    Fix function

--- passkeys/start-registration/api/src/lib/ext-api.ts
+++ passkeys/start-registration/api/src/lib/ext-api.ts
@@ -1,12 +1,13 @@
+import { getFunctions } from '@firebase-with-passkeys/firebase-app-functions';
 import { getClientPasskeysProgram } from '@firebase-with-passkeys/passkeys-program-client';
-import { startRegistrationApi as _startRegistrationApi } from './start-registration-api';
-import { Functions } from 'firebase/functions';
 import * as TE from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
+import { startRegistrationApi as _startRegistrationApi } from './start-registration-api';
 
 export type StartRegistrationApi = TE.TaskEither<Error, void>;
 
-export const startRegistrationApi = (
-  functions: Functions
-): StartRegistrationApi =>
-  pipe(functions, getClientPasskeysProgram, _startRegistrationApi);
+export const startRegistrationApi: StartRegistrationApi = pipe(
+  getFunctions(),
+  getClientPasskeysProgram,
+  _startRegistrationApi
+);
