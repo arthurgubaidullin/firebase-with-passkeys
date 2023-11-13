7469c8d3fedcd79261c068bded88d3746371a0ee
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-13 16:07:31 +0000

    Add component

---
+++ web/src/sign-in-with-email-link/complete/complete-form.tsx
@@ -0,0 +1,26 @@
+import * as FR from '@firebase-with-passkeys/fetch-result-type';
+import { pipe } from 'fp-ts/function';
+import { observer } from 'mobx-react-lite';
+import { createCompleteStore } from './complete-store';
+import { useEffect } from 'react';
+
+const store = createCompleteStore();
+
+export const CompleteSendSignInLinkToEmailForm = observer(() => {
+  useEffect(() => {
+    store.get().complete();
+  }, []);
+  return pipe(
+    store.get(),
+    FR.fold(
+      () => <div />,
+      () => <div>Fetching…</div>,
+      (e) => (
+        <div>
+          Failure: <pre>{e.error.message}</pre>
+        </div>
+      ),
+      () => <div>Success!!1</div>
+    )
+  );
+});
