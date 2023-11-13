69369bc54d58350c5d959b817add589142140f55
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-13 13:31:19 +0000

    Fix function

--- passkeys/start-authentication/api/src/lib/start-authentication-ext-api.ts
+++ passkeys/start-authentication/api/src/lib/start-authentication-ext-api.ts
@@ -1,14 +1,15 @@
+import { getFunctions } from '@firebase-with-passkeys/firebase-app-functions';
 import { getClientPasskeysProgram } from '@firebase-with-passkeys/passkeys-program-client';
-import { Functions } from 'firebase/functions';
+import { TaskEither } from 'fp-ts/TaskEither';
 import { pipe } from 'fp-ts/function';
 import { startAuthenticationApi as _startAuthenticationApi } from './start-authentication-api';
-import { TaskEither } from 'fp-ts/TaskEither';
 
 export type StartAuthenticationApi = (
   username: string
 ) => TaskEither<Error, void>;
 
-export const startAuthenticationApi = (
-  functions: Functions
-): StartAuthenticationApi =>
-  pipe(functions, getClientPasskeysProgram, _startAuthenticationApi);
+export const startAuthenticationApi: StartAuthenticationApi = pipe(
+  getFunctions(),
+  getClientPasskeysProgram,
+  _startAuthenticationApi
+);
