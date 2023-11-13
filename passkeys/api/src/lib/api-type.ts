07d95cd2307f9eebc677b3033eeb21d03eca4abd
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-13 13:33:26 +0000

    Del project

--- passkeys/api/src/lib/api-type.ts
+++
@@ -1,9 +0,0 @@
-import { StartAuthenticationApi as _StartAuthenticationApi } from '@firebase-with-passkeys/passkeys-start-authentication-api';
-import { StartRegistrationApi as _StartRegistrationApi } from '@firebase-with-passkeys/passkeys-start-registration-api';
-
-export interface StartAuthenticationApi {
-  readonly startAuthentication: _StartAuthenticationApi;
-}
-export interface StartRegistrationApi {
-  readonly startRegistration: _StartRegistrationApi;
-}
