930c41d7133ea037037006b8afd840c203ba3c8b
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-13 11:56:46 +0000

    Add method to api

--- passkeys/api/client/src/lib/api.ts
+++ passkeys/api/client/src/lib/api.ts
@@ -1,9 +1,11 @@
 import { Functions } from 'firebase/functions';
 import { startAuthenticationApi } from './start-authentication-api';
 import { PasskeysClientApi } from './api-type';
+import { startRegistrationApi } from '@firebase-with-passkeys/passkeys-start-registration-api';
 
 export const getPasskeysClientApi = (
   functions: Functions
 ): PasskeysClientApi => ({
   startAuthentication: startAuthenticationApi(functions),
+  startRegistration: startRegistrationApi(functions),
 });
