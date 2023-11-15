d90bf0aaf1477c17a6add27bc42504ef8e794a43
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-15 11:27:42 +0000

    Refactor

--- remote-data/get/src/lib/fsm.ts
+++ remote-data/get/src/lib/fsm.ts
@@ -13,28 +13,28 @@ export type { RemoteData } from '@firebase-with-passkeys/remote-data-type';
 export const initial = RD.initial;
 
 export const fetch = <E, A>(
-  r: RD.RemoteData<E, A>
+  rd: RD.RemoteData<E, A>
 ): O.Option<RD.RemoteData<E, A>> =>
   pipe(
-    r,
+    rd,
     O.fromPredicate(RD.isInitial),
     O.map(() => RD.fetching)
   );
 
 export const success =
   <E, A>(a: A) =>
-  (r: RD.RemoteData<E, A>): O.Option<RD.RemoteData<E, A>> =>
+  (rd: RD.RemoteData<E, A>): O.Option<RD.RemoteData<E, A>> =>
     pipe(
-      r,
+      rd,
       O.fromPredicate(RD.isFetching),
       O.map(() => RD.success(a))
     );
 
 export const failure =
   <E, A>(e: E) =>
-  (r: RD.RemoteData<E, A>): O.Option<RD.RemoteData<E, A>> =>
+  (rd: RD.RemoteData<E, A>): O.Option<RD.RemoteData<E, A>> =>
     pipe(
-      r,
+      rd,
       O.fromPredicate(RD.isFetching),
       O.map(() => RD.failure(e))
     );
