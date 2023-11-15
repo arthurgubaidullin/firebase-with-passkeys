2b7643ba5a3696e976bc3063560fc522b0256ffe
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-15 17:51:38 +0000

    Add function

--- auth/create-store-api/src/lib/create-store-api.ts
+++ auth/create-store-api/src/lib/create-store-api.ts
@@ -1,9 +1,18 @@
 import { createStore } from '@firebase-with-passkeys/auth-create-store';
 import * as AuthState from '@firebase-with-passkeys/auth-state';
+import { UserStruct } from '@firebase-with-passkeys/auth-user-struct';
 import { ReadonlyObservable } from '@firebase-with-passkeys/observable-type';
+import {
+  RemoteData,
+  createRealtimeObservable,
+} from '@firebase-with-passkeys/remote-data/realtime-observable';
 import { FirebaseApp } from 'firebase/app';
+import { pipe } from 'fp-ts/function';
 import { action, observable, onBecomeObserved, onBecomeUnobserved } from 'mobx';
-import { onAuthStateChangedAdapter } from './on-auth-state-changed-adapter';
+import {
+  onAuthStateChangedAdapter,
+  onAuthStateChangedAdapter2,
+} from './on-auth-state-changed-adapter';
 
 export const createAuthStateStoreApi = (
   firebaseApp: FirebaseApp
@@ -15,3 +24,8 @@ export const createAuthStateStoreApi = (
     onBecomeObserved,
     onBecomeUnobserved,
   });
+
+export const createAuthStateStoreApi2 = (
+  firebaseApp: FirebaseApp
+): ReadonlyObservable<RemoteData<Error, UserStruct>> =>
+  pipe(firebaseApp, onAuthStateChangedAdapter2, createRealtimeObservable);
