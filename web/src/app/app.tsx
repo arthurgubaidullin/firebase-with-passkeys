45779398646c686aee18e7acf1a9553579038544
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-15 10:57:30 +0000

    Fix component

--- web/src/app/app.tsx
+++ web/src/app/app.tsx
@@ -1,7 +1,22 @@
+import { observer } from 'mobx-react-lite';
+import { createLocationStore } from '../location/create-store';
+import { completeSignIn, signIn } from '../location/location';
+import { CompleteSendSignInLinkToEmailForm } from '../sign-in-with-email-link/complete/complete-form';
+import { SendSignInLinkToEmailForm } from '../sign-in-with-email-link/send-link/send-link-form';
 import './app.module.css';
 
-export function App() {
-  return <div></div>;
-}
+const location = createLocationStore();
+
+export const App = observer(() => {
+  const _location = location.get();
+  if (_location === signIn) {
+    return <SendSignInLinkToEmailForm />;
+  }
+  if (_location === completeSignIn) {
+    return <CompleteSendSignInLinkToEmailForm />;
+  }
+
+  return <div>test</div>;
+});
 
 export default App;
