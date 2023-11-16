a5a071b6493636ff67b9baa718d0d566c0e3683b
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-16 07:05:57 +0000

    Reexport

--- remote-data/api/src/lib/remote-data.ts
+++ remote-data/api/src/lib/remote-data.ts
@@ -1,1 +1,3 @@
 export * from '@firebase-with-passkeys/remote-data-display';
+export * from '@firebase-with-passkeys/remote-data-get-observable';
+export * from '@firebase-with-passkeys/remote-data/realtime-observable';
