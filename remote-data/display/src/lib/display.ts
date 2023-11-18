d0404d0b646268ba3ed62f6f76e96140fd8e4a3d
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-18 10:43:24 +0000

    Fix type

--- remote-data/display/src/lib/display.ts
+++ remote-data/display/src/lib/display.ts
@@ -1,7 +1,42 @@
 import * as RD from '@firebase-with-passkeys/remote-data-type';
 import { absurd } from 'fp-ts/function';
 
-export * from '@firebase-with-passkeys/remote-data-type';
+const INITIAL = 'initial' as const;
+const FETCHING = 'fetching' as const;
+const SUCCESS = 'success' as const;
+const FAILURE = 'failure' as const;
+
+interface Initial {
+  readonly _tag: typeof INITIAL;
+}
+
+interface Fetching {
+  readonly _tag: typeof FETCHING;
+}
+
+interface Success<A> {
+  readonly _tag: typeof SUCCESS;
+  readonly data: A;
+}
+
+interface Failure<E> {
+  readonly _tag: typeof FAILURE;
+  readonly error: E;
+}
+
+export type RemoteData<E, A> = Initial | Fetching | Success<A> | Failure<E>;
+
+export const isInitial = <E, A>(rd: RemoteData<E, A>): rd is Initial =>
+  rd._tag === INITIAL;
+
+export const isFetching = <E, A>(rd: RemoteData<E, A>): rd is Fetching =>
+  rd._tag === FETCHING;
+
+export const isSuccess = <E, A>(rd: RemoteData<E, A>): rd is Success<A> =>
+  rd._tag === SUCCESS;
+
+export const isFailure = <E, A>(rd: RemoteData<E, A>): rd is Failure<E> =>
+  rd._tag === FAILURE;
 
 export const fold =
   <E, A, B>(
