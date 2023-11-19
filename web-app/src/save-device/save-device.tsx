2ff1ee30fcc2ae91a2aa12dd1cfcb96ad23d3bba
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-19 17:34:53 +0000

    Fix component

--- web-app/src/save-device/save-device.tsx
+++ web-app/src/save-device/save-device.tsx
@@ -1,9 +1,22 @@
+import { createGetObservable, fold } from '@firebase-with-passkeys/remote-data';
+import { startRegistrationApi } from '@firebase-with-passkeys/passkeys-start-registration-api';
 import { useId } from 'react';
+import { pipe } from 'fp-ts/function';
+
+const startRegistrationStore = createGetObservable(() => startRegistrationApi);
 
 export const SaveDeviceForm = () => {
   const id = useId();
-  return (
-    <form>
+  const process = startRegistrationStore.get();
+
+  const form = (
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault();
+
+        await process.fetch(void 0);
+      }}
+    >
       <div>
         <label htmlFor={id}>Save device?</label>
         <input id={id} type="checkbox" required />
@@ -12,5 +25,19 @@ export const SaveDeviceForm = () => {
         <input type="submit" />
       </div>
     </form>
+  );
+
+  return pipe(
+    process,
+    fold(
+      () => form,
+      () => <div>Fetching…</div>,
+      (e) => (
+        <div>
+          Failure: <pre>{e.message}</pre>
+        </div>
+      ),
+      () => <div>Success!!1</div>
+    )
   );
 };
