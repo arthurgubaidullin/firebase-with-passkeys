3697192f84f01e3c69914727e41b957cdd4e6dc1
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-19 20:02:14 +0000

    Fix types

--- passkeys/challenge/get-document/src/lib/get-document-type.ts
+++ passkeys/challenge/get-document/src/lib/get-document-type.ts
@@ -1,8 +1,9 @@
 import { ChallengeDocument } from '@firebase-with-passkeys/passkeys-challenge-document';
 import * as O from 'fp-ts/Option';
+import { OutputOf } from 'io-ts';
 
 export interface GetChallenge {
   readonly getChallenge: (
     userId: string
-  ) => Promise<O.Option<ChallengeDocument>>;
+  ) => Promise<O.Option<OutputOf<typeof ChallengeDocument>>>;
 }
--- passkeys/challenge/set-document/src/lib/set-challenge-type.ts
+++ passkeys/challenge/set-document/src/lib/set-challenge-type.ts
@@ -1,8 +1,9 @@
 import { ChallengeDocument } from '@firebase-with-passkeys/passkeys-challenge-document';
+import { OutputOf } from 'io-ts';
 
 export interface SetChallenge {
   readonly setChallenge: (
     userId: string,
-    challenge: ChallengeDocument
+    challenge: OutputOf<typeof ChallengeDocument>
   ) => Promise<void>;
 }
