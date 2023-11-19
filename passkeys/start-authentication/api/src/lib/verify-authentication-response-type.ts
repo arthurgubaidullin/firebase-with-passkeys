d1cffd0d72c74b15a25e1e4b5ba2ac24c587adde
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-19 20:14:06 +0000

    Fix type

--- passkeys/start-authentication/api/src/lib/verify-authentication-response-type.ts
+++ passkeys/start-authentication/api/src/lib/verify-authentication-response-type.ts
@@ -1,5 +1,7 @@
+import { RequestData } from '@firebase-with-passkeys/passkeys-verify-authentication-response-contract';
+
 export interface VerifyAuthenticationResponse {
-  readonly verifyAuthenticationResponse: (data?: unknown) => Promise<{
+  readonly verifyAuthenticationResponse: (data: RequestData) => Promise<{
     data?: unknown;
   }>;
 }
