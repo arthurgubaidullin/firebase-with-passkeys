c2123e3f483e569f0377ec538e0de62aea52434d
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-19 17:06:39 +0000

    Fix store

--- web-app/src/current-user/current-user-store.ts
+++ web-app/src/current-user/current-user-store.ts
@@ -1,5 +1,5 @@
-import { createAuthStateStoreApi } from '@firebase-with-passkeys/auth-create-store-api';
+import { createAuthStateStoreApi2 } from '@firebase-with-passkeys/auth-create-store-api';
 import { getFirebaseApp } from '@firebase-with-passkeys/firebase-app-instance';
 
 export const createCurrentUser = () =>
-  createAuthStateStoreApi(getFirebaseApp());
+  createAuthStateStoreApi2(getFirebaseApp());
