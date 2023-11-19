f800c0616c7adda1f12e7cd622b0790932c0f689
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-19 20:02:32 +0000

    Fix type

--- auth/user-struct/src/lib/user-struct.ts
+++ auth/user-struct/src/lib/user-struct.ts
@@ -1,8 +1,10 @@
 import * as O from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { NonEmptyString } from 'io-ts-types';
 
 export interface UserStruct {
   readonly uid: string;
-  readonly email: O.Option<string>;
+  readonly email: O.Option<NonEmptyString>;
 }
 
 export const fromUserRecord = (a: {
@@ -10,5 +12,8 @@ export const fromUserRecord = (a: {
   email?: string | null;
 }): UserStruct => ({
   uid: a.uid,
-  email: O.fromNullable(a.email),
+  email: pipe(
+    O.fromNullable(a.email),
+    O.chain(O.fromPredicate(NonEmptyString.is))
+  ),
 });
