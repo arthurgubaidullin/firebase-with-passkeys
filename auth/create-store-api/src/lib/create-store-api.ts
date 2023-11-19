369f76da7ae4c9f230e8a66beec55816b8cbaea1
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-19 17:20:33 +0000

    Remove function

--- auth/create-store-api/src/lib/create-store-api.ts
+++ auth/create-store-api/src/lib/create-store-api.ts
@@ -1,30 +1,13 @@
-import { createStore } from '@firebase-with-passkeys/auth-create-store';
-import * as AuthState from '@firebase-with-passkeys/auth-state';
 import { UserStruct } from '@firebase-with-passkeys/auth-user-struct';
 import { ReadonlyObservable } from '@firebase-with-passkeys/observable-type';
 import { createRealtimeObservable } from '@firebase-with-passkeys/remote-data';
-import { FirebaseApp } from 'firebase/app';
-import { pipe } from 'fp-ts/function';
-import { action, observable, onBecomeObserved, onBecomeUnobserved } from 'mobx';
-import {
-  onAuthStateChangedAdapter,
-  onAuthStateChangedAdapter2,
-} from './on-auth-state-changed-adapter';
 import { RemoteData } from '@firebase-with-passkeys/remote-data-display';
+import { FirebaseApp } from 'firebase/app';
 import { Option } from 'fp-ts/Option';
+import { pipe } from 'fp-ts/function';
+import { onAuthStateChangedAdapter2 } from './on-auth-state-changed-adapter';
 
 export const createAuthStateStoreApi = (
-  firebaseApp: FirebaseApp
-): ReadonlyObservable<AuthState.AuthState> =>
-  createStore({
-    onAuthStateChanged: onAuthStateChangedAdapter(firebaseApp),
-    observable: (a) => observable.box(a, { deep: false }),
-    action,
-    onBecomeObserved,
-    onBecomeUnobserved,
-  });
-
-export const createAuthStateStoreApi2 = (
   firebaseApp: FirebaseApp
 ): ReadonlyObservable<RemoteData<Error, Option<UserStruct>>> =>
   pipe(firebaseApp, onAuthStateChangedAdapter2, createRealtimeObservable);
--- web-app/src/current-user/current-user-store.ts
+++ web-app/src/current-user/current-user-store.ts
@@ -1,5 +1,5 @@
-import { createAuthStateStoreApi2 } from '@firebase-with-passkeys/auth-create-store-api';
+import { createAuthStateStoreApi } from '@firebase-with-passkeys/auth-create-store-api';
 import { getFirebaseApp } from '@firebase-with-passkeys/firebase-app-instance';
 
 export const getCurrentUserStore = () =>
-  createAuthStateStoreApi2(getFirebaseApp());
+  createAuthStateStoreApi(getFirebaseApp());
