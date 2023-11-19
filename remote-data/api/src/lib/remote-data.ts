556f686abdbaedf48407c2253f93bd8d35db1a4e
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-19 12:10:45 +0000

    Refactor

--- remote-data/api/src/lib/remote-data.ts
+++ remote-data/api/src/lib/remote-data.ts
@@ -1,4 +1,5 @@
 import { createGetObservable as _createGetObservable } from '@firebase-with-passkeys/remote-data-get-observable';
+import { createRealtimeObservable as _createRealtimeObservable } from '@firebase-with-passkeys/remote-data-realtime-observable';
 import {
   action,
   computed,
@@ -7,12 +8,14 @@ import {
   onBecomeUnobserved,
 } from 'mobx';
 export * from '@firebase-with-passkeys/remote-data-display';
-export * from '@firebase-with-passkeys/remote-data-realtime-observable';
 
-export const createGetObservable = _createGetObservable({
+const program = {
   action,
   computed,
-  observable: (v) => observable.box(v, { deep: false }),
+  observable: <T>(v: T) => observable.box(v, { deep: false }),
   onBecomeObserved,
   onBecomeUnobserved,
-});
+};
+export const createGetObservable = _createGetObservable(program);
+
+export const createRealtimeObservable = _createRealtimeObservable(program);
--- remote-data/realtime-observable/src/lib/realtime-observable.ts
+++ remote-data/realtime-observable/src/lib/realtime-observable.ts
@@ -1,77 +1,89 @@
 import * as FSM from '@firebase-with-passkeys/remote-data-realtime-fsm';
 import * as O from 'fp-ts/Option';
 import { constVoid, pipe } from 'fp-ts/function';
-import { action, observable, onBecomeObserved, onBecomeUnobserved } from 'mobx';
 import { Observer } from './observer-type';
 import { ReadonlyObservable } from './readonly-observable-type';
 import { Unsubscribe } from './unsubscribe-type';
 import { RemoteData } from '@firebase-with-passkeys/remote-data-display';
+import {
+  CreateObservable,
+  CreateComputedObservable,
+  CreateAction,
+  OnBecomeObserved,
+  OnBecomeUnobserved,
+} from '@firebase-with-passkeys/observable-type';
 
-export const createRealtimeObservable = <E, A>(
-  subscribe: (observer: Observer<E, A>) => Unsubscribe
-): ReadonlyObservable<RemoteData<E, A>> => {
-  const box = observable.box<RemoteData<E, A>>(FSM.initial, {
-    deep: false,
-  });
+export const createRealtimeObservable =
+  (
+    P: CreateObservable &
+      CreateComputedObservable &
+      CreateAction &
+      OnBecomeObserved &
+      OnBecomeUnobserved
+  ) =>
+  <E, A>(
+    subscribe: (observer: Observer<E, A>) => Unsubscribe
+  ): ReadonlyObservable<RemoteData<E, A>> => {
+    const box = P.observable<RemoteData<E, A>>(FSM.initial);
 
-  const fetch: () => O.Option<void> = action(() =>
-    pipe(
-      box.get(),
-      FSM.fetch,
-      O.map((r) => box.set(r))
-    )
-  );
-  const successOrNext: (a: A) => O.Option<void> = action((a: A) =>
-    pipe(
-      box.get(),
-      FSM.success<E, A>(a),
-      O.map((r) => box.set(r))
-    )
-  );
-  const failure: (e: E) => O.Option<void> = action((e: E) =>
-    pipe(
-      box.get(),
-      FSM.failure<E, A>(e),
-      O.map((r) => box.set(r))
-    )
-  );
-
-  const _subscribe = (): Unsubscribe =>
-    pipe(
-      fetch(),
-      O.map(
-        (): Unsubscribe =>
-          subscribe({
-            next: (a) => {
-              successOrNext(a);
-            },
-            error: (e) => {
-              failure(e);
-            },
-            complete: () => {
-              box.set(FSM.initial);
-            },
-          })
-      ),
-      O.map(
-        (f): Unsubscribe =>
-          () => {
-            box.set(FSM.initial);
-            f();
-          }
-      ),
-      O.getOrElse(() => constVoid)
+    const fetch: () => O.Option<void> = P.action(() =>
+      pipe(
+        box.get(),
+        FSM.fetch,
+        O.map((r) => box.set(r))
+      )
+    );
+    const successOrNext: (a: A) => O.Option<void> = P.action((a: A) =>
+      pipe(
+        box.get(),
+        FSM.success<E, A>(a),
+        O.map((r) => box.set(r))
+      )
+    );
+    const failure: (e: E) => O.Option<void> = P.action((e: E) =>
+      pipe(
+        box.get(),
+        FSM.failure<E, A>(e),
+        O.map((r) => box.set(r))
+      )
     );
 
-  let unsubscribe: Unsubscribe;
+    const _subscribe = (): Unsubscribe =>
+      pipe(
+        fetch(),
+        O.map(
+          (): Unsubscribe =>
+            subscribe({
+              next: (a) => {
+                successOrNext(a);
+              },
+              error: (e) => {
+                failure(e);
+              },
+              complete: () => {
+                box.set(FSM.initial);
+              },
+            })
+        ),
+        O.map(
+          (f): Unsubscribe =>
+            () => {
+              box.set(FSM.initial);
+              f();
+            }
+        ),
+        O.getOrElse(() => constVoid)
+      );
 
-  onBecomeObserved(box, () => {
-    unsubscribe = _subscribe();
-  });
+    let unsubscribe: Unsubscribe;
 
-  onBecomeUnobserved(box, () => {
-    unsubscribe();
-  });
+    P.onBecomeObserved(box, () => {
+      unsubscribe = _subscribe();
+    });
 
-  return box;
-};
+    P.onBecomeUnobserved(box, () => {
+      unsubscribe();
+    });
+
+    return box;
+  };
