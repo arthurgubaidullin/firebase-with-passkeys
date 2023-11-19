9e4b2d44133d0e3a4c5774dda39a5bf3b0f1b890
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-19 17:22:51 +0000

    Remove project

--- auth/create-store/src/lib/on-auth-state-changed-type.ts
+++
@@ -1,9 +0,0 @@
-import { UserStruct } from '@firebase-with-passkeys/auth-user-struct';
-import { Unsubscribe } from './unsubscribe-type';
-import { Option } from 'fp-ts/Option';
-
-export interface OnAuthStateChanged {
-  readonly onAuthStateChanged: (
-    f: (user: Option<UserStruct>) => void
-  ) => Unsubscribe;
-}
