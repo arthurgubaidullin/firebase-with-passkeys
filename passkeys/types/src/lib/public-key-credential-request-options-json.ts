e1a2530b1d3595046694426983f9e155ea0cda55
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-19 18:57:47 +0000

    Fix codec

--- passkeys/types/src/lib/public-key-credential-request-options-json.ts
+++ passkeys/types/src/lib/public-key-credential-request-options-json.ts
@@ -3,6 +3,16 @@ import { Base64URLString } from './base64-url-string';
 import { PublicKeyCredentialDescriptorJSON } from './public-key-credential-descriptor-json';
 import { UserVerificationRequirement } from './user-verification-requirement';
 import { AuthenticationExtensionsClientInputs } from './authentication-extensions-client-inputs';
+import { PublicKeyCredentialRequestOptionsJSON as _PublicKeyCredentialRequestOptionsJSON } from '@simplewebauthn/typescript-types';
+
+type PublicKeyCredentialRequestOptionsJSON = t.TypeOf<
+  typeof PublicKeyCredentialRequestOptionsJSON
+>;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const id = (
+  a: PublicKeyCredentialRequestOptionsJSON
+): _PublicKeyCredentialRequestOptionsJSON => a;
 
 export const PublicKeyCredentialRequestOptionsJSON = t.readonly(
   t.intersection([
