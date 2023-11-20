5e27e825c39c292e42504aafb70acfb4ade1e30a
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-20 09:52:10 +0000

    Fix type

--- passkeys/authenticator/repository/type/src/lib/update-authenticator-type.ts
+++ passkeys/authenticator/repository/type/src/lib/update-authenticator-type.ts
@@ -1,9 +1,10 @@
 import { AuthenticatorDocument } from '@firebase-with-passkeys/passkeys-authenticator-document';
+import { Option } from 'fp-ts/Option';
 
 export interface UpdateAuthenticator {
   readonly updateAuthenticator: (
     userId: string,
     authenticatorId: string,
-    updatedAt: number
+    updatedAt: Option<number>
   ) => (authenticator: AuthenticatorDocument) => Promise<void>;
 }
