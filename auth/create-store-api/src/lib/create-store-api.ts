12fef02301400f825bcbdeddaf70e4f63e7c1e13
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-20 13:49:10 +0000

    Fix functions

--- auth/create-store-api/src/lib/create-store-api.ts
+++ auth/create-store-api/src/lib/create-store-api.ts
@@ -2,12 +2,10 @@ import { UserStruct } from '@firebase-with-passkeys/auth-user-struct';
 import { ReadonlyObservable } from '@firebase-with-passkeys/observable-type';
 import { createRealtimeObservable } from '@firebase-with-passkeys/remote-data';
 import { RemoteData } from '@firebase-with-passkeys/remote-data-display';
-import { FirebaseApp } from 'firebase/app';
 import { Option } from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
 import { onAuthStateChangedAdapter2 } from './on-auth-state-changed-adapter';
 
-export const createAuthStateStoreApi =
-  (firebaseApp: FirebaseApp) =>
-  (): ReadonlyObservable<RemoteData<Error, Option<UserStruct>>> =>
-    pipe(firebaseApp, onAuthStateChangedAdapter2, createRealtimeObservable);
+export const createAuthStateStoreApi = (): ReadonlyObservable<
+  RemoteData<Error, Option<UserStruct>>
+> => pipe(onAuthStateChangedAdapter2, createRealtimeObservable);
--- auth/create-store-api/src/lib/on-auth-state-changed-adapter.ts
+++ auth/create-store-api/src/lib/on-auth-state-changed-adapter.ts
@@ -2,33 +2,34 @@ import {
   UserStruct,
   fromUserRecord,
 } from '@firebase-with-passkeys/auth-user-struct';
+import { getAuth } from '@firebase-with-passkeys/firebase-app-auth';
 import { Observer } from '@firebase-with-passkeys/remote-data-realtime-observable';
-import { FirebaseApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { onAuthStateChanged } from 'firebase/auth';
+import { IO } from 'fp-ts/IO';
 import * as O from 'fp-ts/Option';
 import { pipe } from 'fp-ts/function';
-import { IO } from 'fp-ts/IO';
 
-export const onAuthStateChangedAdapter =
-  (firebaseApp: FirebaseApp) => (f: (user: O.Option<UserStruct>) => void) => {
-    const auth = getAuth(firebaseApp);
-    return onAuthStateChanged(auth, (user) =>
-      pipe(user, O.fromNullable, O.map(fromUserRecord), f)
-    );
-  };
+export const onAuthStateChangedAdapter = (
+  f: (user: O.Option<UserStruct>) => void
+) => {
+  const auth = getAuth();
+  return onAuthStateChanged(auth, (user) =>
+    pipe(user, O.fromNullable, O.map(fromUserRecord), f)
+  );
+};
 
-export const onAuthStateChangedAdapter2 =
-  (firebaseApp: FirebaseApp) =>
-  (observer: Observer<Error, O.Option<UserStruct>>): IO<void> => {
-    const auth = getAuth(firebaseApp);
-    return onAuthStateChanged(auth, {
-      next: (user): void =>
-        pipe(user, O.fromNullable, O.map(fromUserRecord), (u) =>
-          observer.next?.(u)
-        ),
-      error: (e): void => {
-        observer.error?.(e);
-      },
-      complete: (): void => observer.complete?.(),
-    });
-  };
+export const onAuthStateChangedAdapter2 = (
+  observer: Observer<Error, O.Option<UserStruct>>
+): IO<void> => {
+  const auth = getAuth();
+  return onAuthStateChanged(auth, {
+    next: (user): void =>
+      pipe(user, O.fromNullable, O.map(fromUserRecord), (u) =>
+        observer.next?.(u)
+      ),
+    error: (e): void => {
+      observer.error?.(e);
+    },
+    complete: (): void => observer.complete?.(),
+  });
+};
