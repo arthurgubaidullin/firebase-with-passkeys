86dd3de972e6dadea42b226a3a27d717669f4b8c
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-20 09:51:29 +0000

    Fix function

--- passkeys/authenticator/create-document/src/lib/create-authenticator.ts
+++ passkeys/authenticator/create-document/src/lib/create-authenticator.ts
@@ -1,6 +1,7 @@
 import { AuthenticatorDocument } from '@firebase-with-passkeys/passkeys-authenticator-document';
 import { CreateAuthenticator } from '@firebase-with-passkeys/passkeys-authenticator-repository-type';
 import { OutputOf } from 'io-ts';
+import { Buffer } from 'node:buffer';
 
 export const createAuthenticatorDocument =
   (P: CreateAuthenticator) =>
@@ -9,5 +10,8 @@ export const createAuthenticatorDocument =
     authenticator: OutputOf<typeof AuthenticatorDocument>
   ) => {
     const data = AuthenticatorDocument.encode(authenticator);
-    await P.createAuthenticator(userId, data.credentialID.toString(), data);
+    const authenticatorId = Buffer.from(data.credentialID).toString(
+      'base64url'
+    );
+    await P.createAuthenticator(userId, authenticatorId, data);
   };
