b09285d21074b997272470e0040d657a4e697e40
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-20 12:09:19 +0000

    Fix function

--- passkeys/program/client/src/lib/program.ts
+++ passkeys/program/client/src/lib/program.ts
@@ -2,22 +2,32 @@ import { getGenerateAuthenticationOptionsFirebaseFunction } from '@firebase-with
 import { getGenerateRegistrationOptionsFirebaseFunction } from '@firebase-with-passkeys/passkeys-generate-registration-options-firebase-function-client';
 import { getVerifyAuthenticationResponseFirebaseFunction } from '@firebase-with-passkeys/passkeys-verify-authentication-response-firebase-function-client';
 import { getVerifyRegistrationResponseFirebaseFunction } from '@firebase-with-passkeys/passkeys-verify-registration-response-firebase-function-client';
+import { Auth, signInWithCustomToken } from 'firebase/auth';
 import { Functions, HttpsCallable } from 'firebase/functions';
+import * as TE from 'fp-ts/TaskEither';
+import * as E from 'fp-ts/Either';
 
-type ClientProgram = Readonly<{
-  generateRegistrationOptions: HttpsCallable<unknown, unknown>;
-  generateAuthenticationOptions: HttpsCallable<unknown, unknown>;
-  verifyRegistrationResponse: HttpsCallable<unknown, unknown>;
-  verifyAuthenticationResponse: HttpsCallable<unknown, unknown>;
-}>;
+interface ClientProgram {
+  readonly generateRegistrationOptions: HttpsCallable<unknown, unknown>;
+  readonly generateAuthenticationOptions: HttpsCallable<unknown, unknown>;
+  readonly verifyRegistrationResponse: HttpsCallable<unknown, unknown>;
+  readonly verifyAuthenticationResponse: HttpsCallable<unknown, unknown>;
+  readonly signInWithCustomToken: (token: string) => TE.TaskEither<Error, void>;
+}
 
-export const getClientPasskeysProgram = (
-  functions: Functions
-): ClientProgram => {
+export const getClientPasskeysProgram = (data: {
+  functions: Functions;
+  auth: Auth;
+}): ClientProgram => {
   return {
-    ...getGenerateAuthenticationOptionsFirebaseFunction(functions),
-    ...getGenerateRegistrationOptionsFirebaseFunction(functions),
-    ...getVerifyRegistrationResponseFirebaseFunction(functions),
-    ...getVerifyAuthenticationResponseFirebaseFunction(functions),
+    ...getGenerateAuthenticationOptionsFirebaseFunction(data.functions),
+    ...getGenerateRegistrationOptionsFirebaseFunction(data.functions),
+    ...getVerifyRegistrationResponseFirebaseFunction(data.functions),
+    ...getVerifyAuthenticationResponseFirebaseFunction(data.functions),
+    signInWithCustomToken: (t) => {
+      return TE.tryCatch(async () => {
+        await signInWithCustomToken(data.auth, t);
+      }, E.toError);
+    },
   };
 };
