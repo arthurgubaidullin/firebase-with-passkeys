90c4c28c613f8a8907e6e51a1e74e394554c7c87
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-20 10:29:57 +0000

    Fix function

--- passkeys/verify-authentication-response/use-case/src/lib/verify-authentication-response.ts
+++ passkeys/verify-authentication-response/use-case/src/lib/verify-authentication-response.ts
@@ -122,7 +122,7 @@ export const verifyAuthenticationResponse =
 
     await updateAuthenticatorCounter(P)(
       u.value.uid,
-      u.value.uid,
+      response.id,
       newCounter,
       updatedAt
     )(authenticator);
