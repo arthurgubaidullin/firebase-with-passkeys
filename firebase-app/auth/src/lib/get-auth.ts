4e08211629ea1b16de9fc8a0fb213124450ac475
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-21 09:57:26 +0000

    Fix function

--- firebase-app/auth/src/lib/get-auth.ts
+++ firebase-app/auth/src/lib/get-auth.ts
@@ -1,14 +1,18 @@
 import { getFirebaseApp } from '@firebase-with-passkeys/firebase-app-instance';
-import { getAuth as _getAuth, connectAuthEmulator } from 'firebase/auth';
+import { Auth, getAuth as _getAuth, connectAuthEmulator } from 'firebase/auth';
 
 const LOCAL_HOSTS = ['127.0.0.1', 'localhost'];
 const EMULATOR_HOST = 'http://127.0.0.1:9099';
 
+let auth: Auth | null = null;
+
 export const getAuth = () => {
-  const app = getFirebaseApp();
-  const auth = _getAuth(app);
-  if (LOCAL_HOSTS.includes(window.location.hostname)) {
-    connectAuthEmulator(auth, EMULATOR_HOST);
+  if (!auth) {
+    const app = getFirebaseApp();
+    auth = _getAuth(app);
+    if (LOCAL_HOSTS.includes(window.location.hostname)) {
+      connectAuthEmulator(auth, EMULATOR_HOST);
+    }
   }
   return auth;
 };
