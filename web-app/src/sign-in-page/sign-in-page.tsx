64cdf3e9805fed19781e02160637b4f6a87e044f
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-21 09:41:10 +0000

    Fix component

--- web-app/src/sign-in-page/sign-in-page.tsx
+++ web-app/src/sign-in-page/sign-in-page.tsx
@@ -1,17 +1,34 @@
+import * as RD from '@firebase-with-passkeys/remote-data';
+import { pipe } from 'fp-ts/function';
 import { observer } from 'mobx-react-lite';
-import { SignInWithPasskey } from '../sign-in-with-passkey/sign-in-with-passkey';
+import { getCurrentUserStore } from '../current-user/current-user-store';
 import { SendSignInLinkToEmailForm } from '../sign-in-with-email-link/send-link/send-link-form';
+import { SignInWithPasskey } from '../sign-in-with-passkey/sign-in-with-passkey';
+import * as O from 'fp-ts/Option';
+
+const currentUserStore = getCurrentUserStore();
 
 export const SignInPage = observer(() => {
-  return (
-    <div>
-      <h1>Sign in</h1>
-      <div>
-        <SignInWithPasskey />
-      </div>
-      <div>
-        <SendSignInLinkToEmailForm />
-      </div>
-    </div>
+  return pipe(
+    currentUserStore.get(),
+    RD.fold(
+      () => <div>Loading…</div>,
+      () => <div>Loading…</div>,
+      (e) => <div>Failure: {e.message}</div>,
+      O.fold(
+        () => (
+          <div>
+            <h1>Sign in</h1>
+            <div>
+              <SignInWithPasskey />
+            </div>
+            <div>
+              <SendSignInLinkToEmailForm />
+            </div>
+          </div>
+        ),
+        () => <div>Your are authorized!</div>
+      )
+    )
   );
 });
