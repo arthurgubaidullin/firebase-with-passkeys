53ff4cb81305693f5274c85e6c90d419ca730cca
Artur Gubaidullin <lapteuh@gmail.com>
2023-11-28 13:57:24 +0000

    Fix component

--- web-app/src/sign-in-with-email-link/send-link/send-link-form.tsx
+++ web-app/src/sign-in-with-email-link/send-link/send-link-form.tsx
@@ -1,51 +1,96 @@
 import { createSendLinkStore } from '@firebase-with-passkeys/auth-sign-in-with-email-link';
-import { fold } from '@firebase-with-passkeys/remote-data';
-import { pipe } from 'fp-ts/function';
+import * as RD from '@firebase-with-passkeys/remote-data';
 import { observer } from 'mobx-react-lite';
-import { useId } from 'react';
 
 const URL: string = `${import.meta.env['VITE_HOST'] ?? ''}/sign-in/complete`;
 
 const store = createSendLinkStore(URL);
 
 export const SendSignInLinkToEmailForm = observer(() => {
-  const id = useId();
   const state = store.get();
-  const form = (
-    <form
-      onSubmit={async (e) => {
-        e.preventDefault();
-        const fd = new FormData(e.currentTarget);
-        const email = fd.get('email')?.toString() ?? '';
-        await state.send(email);
-      }}
-    >
-      <div>
-        <label htmlFor={id}>Email:</label>
-        <input id={id} type="email" datatype="email" name="email" required />
-      </div>
-      <div>
-        <input type="submit" />
-      </div>
-    </form>
-  );
   return (
-    <div>
-      <h2>Sign in with email link</h2>
-      <div>
-        {pipe(
-          state,
-          fold(
-            () => form,
-            () => <div>Fetching…</div>,
-            (e) => (
-              <div>
-                Failure: <pre>{e.message}</pre>
-              </div>
-            ),
-            () => <div>Success!!1</div>
-          )
-        )}
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form
+          className="space-y-6"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const fd = new FormData(e.currentTarget);
+            const email = fd.get('email')?.toString() ?? '';
+            await state.send(email);
+          }}
+        >
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-2">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                autoComplete="email"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                disabled={!RD.isInitial(state)}
+              />
+            </div>
+          </div>
+
+          <div>
+            {RD.isInitial(state) && (
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Send email link
+              </button>
+            )}
+
+            {RD.isFetching(state) && (
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                disabled
+              >
+                Processing...
+              </button>
+            )}
+
+            {RD.isSuccess(state) && (
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                disabled
+              >
+                Success! Check your inbox.
+              </button>
+            )}
+
+            {RD.isFailure(state) && (
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                disabled
+              >
+                Failed! Reload page.
+              </button>
+            )}
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          This is a demonstration project.
+        </p>
       </div>
     </div>
   );
